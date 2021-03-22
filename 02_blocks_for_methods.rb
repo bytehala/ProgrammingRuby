9ff2e2c55e2df8b5756d29c3d0105d3554922bde
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-03-21 19:23:55 -0600

    Blocks too

--- 02_blocks_for_methods.rb
+++ 02_blocks_for_methods.rb
@@ -6,3 +6,19 @@ def greet(name)
 end
 
 greet("Alex") {puts "A block associated with greet"}
+
+# Passing variables to yield:
+
+def pass_vars
+  yield("Alex", "What's up")
+end
+
+pass_vars {|name, phrase| puts "#{name} says #{phrase}"}
+
+
+['cat', 'dog', 'horse'].each {|name| print name, " " }
+
+5.times {  print "*" }
+3.upto(6) {|i|  print i }
+('a'..'e').each {|char| print char }
+puts
