03af0d82168f5b2a333edd3fde9a03144ac78f51
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-03-23 21:17:19 -0600

    Refactor hash wordcount

--- 03_hash_word_count.rb
+++ 03_hash_word_count.rb
@@ -1,12 +1,11 @@
 
 sentence = "The quick brown fox jumps over the lazy dog"
 
-# TODO regex should allow for an apostrophe
-array = sentence.split(/[\W']+/)
+array = sentence.downcase.scan(/[\w']+/)
 
 h = Hash.new(0)
 array.each do |w|
-  h[w.downcase] = h[w.downcase]+1
+  h[w] += 1
 end
 
 p h
